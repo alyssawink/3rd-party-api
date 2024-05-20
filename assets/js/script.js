7d3415d8c14ee276cd196ee9e890c94171df53bf
Alyssa H <alyssa.hanewinkel@gmail.com>
2024-05-19 22:03:13 -0500

    created a function to handle dropping a task into a new status lane

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,7 +123,20 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+    const taskId = ui.draggable.attr('data-task-id');
+    const tasks = taskList;
+    console.log(taskId);
 
+    const newStatus = event.target.id;
+    console.log(newStatus);
+
+    for (let task of tasks) {
+        if(task.id == taskId) {
+            task.status = newStatus;
+        }
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
