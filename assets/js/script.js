f8c755be7989ff99631600375e544aa9f1fce838
Alyssa H <alyssa.hanewinkel@gmail.com>
2024-05-19 21:34:36 -0500

    created a function to create a task card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,43 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+let nextId = JSON.parse(localStorage.getItem("nextId")) || 1;
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
-}
+    let uniqueId = Date.now();
+    return uniqueId;
+};
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = $('<div>')
+        .addClass('card project-card draggable my-3')
+        .attr('data-task-id', task.id);
+    const cardHeader = $('<div>').addClass('card-header').text(task.name);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.description);
+    const cardDeadLine = $('<p>').addClass('card-text').text(task.deadline);
+    const cardDeleteBtn = $('<button>')
+        .addClass('btn btn-danger delete')
+        .text('Delete')
+        .attr('data-task-id', task.id);
+    cardDeleteBtn.on('click', handleDeleteTask);
 
+    if (task.deadline && task.status !== 'done') {
+        const now = dayjs();
+        const deadline = dayjs(task.deadline, 'DD/MM/YYY');
+
+        if (now.isSame(deadline, 'day')) {
+            taskCard.addClass('bg-warning text-white');
+        } else if (now.isSame(deadline, 'day')) {
+            taskCard.addClass('bg-danger text-white');
+            cardDeleteBtn.addClass('border-light');
+        }
+    }
+    cardBody.append(cardDescription, cardDeadline, cardDeleteBtn);
+    taskCard.append(cardHeader, cardBody);
+
+    return taskCard
 }
 
 // Todo: create a function to render the task list and make cards draggable
